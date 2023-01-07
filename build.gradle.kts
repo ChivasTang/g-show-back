59020ef345ed41b413ebc9414531d3ab1c868129
tangzh <tangzh1983@gmail.com>
2023-01-07 22:39:10 +0900

    [g-show-back]init project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 }
 
 group = "com.gshow"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+version = "1.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_18
 
 configurations {
     compileOnly {
@@ -25,7 +25,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("com.oracle.database.jdbc:ojdbc8")
+    runtimeOnly("com.oracle.database.jdbc:ojdbc11")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
